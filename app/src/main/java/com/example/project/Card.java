c9afd75e82af9df4fe07725b42e7f3efb980533e
yahliken <yahli.kenzin@gmail.com>
2025-12-07 11:01:45 +0200

    הוספת פעולת draw

--- app/src/main/java/com/example/project/Card.java
+++ app/src/main/java/com/example/project/Card.java
@@ -1,12 +1,14 @@
 package com.example.project;
 
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
 
 public class Card {
 
     private String type;
     private Bitmap bFront;
     private Bitmap bBack;
+    private float x, y;
 
     public Card(String type, Bitmap bFront, Bitmap bBack) {
         this.type = type;
@@ -24,5 +26,25 @@ public class Card {
 
     public Bitmap getbBack() {
         return bBack;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public void draw(Canvas canvas){
+        canvas.drawBitmap(bBack,x,y,null);
     }
 }
